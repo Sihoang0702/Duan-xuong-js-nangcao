8c0f4339f36e2b0ca7baf454ab8c23ccdb68c5b0
hoa phung <hoaptph29645@fpt.edu.vn>
2023-07-15 20:19:47 +0700

    header+footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+<footer class="container mx-auto bg-slate-50">
+    <div class="flex flex-row">
+      <div class="basis-1/3 px-20" >
+          <p class="text-lg font-medium">INFORMATION</p>
+          <li>Về chúng tôi</li>
+          <li>Về chúng tôi</li>
+          <li>Các điều khoản và điều kiện</li>
+          <li>Chính sách bảo mật </li>
+      </div>
+      <div class="basis-1/3 px-20">
+          <p class="text-lg font-medium">SOCIALIZE</p>
+          <li>Facebook</li>
+          <li>Instagram</li>
+      </div>
+      <div class="basis-1/3 px-20">
+          <p class="text-lg font-medium">CUSTOMER SERVICE</p>
+          <li>Liên hệ chúng tôi </li>
+          <li>Phiếu quà tặng </li>
+          <li>Câu hỏi thường gặp </li>
+          <li>Thông tin vận chuyển </li>
+      </div>
+    </div>
+ </footer>
---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+
+
+// nếu ai code bỏ comment dòng 7 và 15 vì ko biết tại sao báo lỗi 
+    <header class="container mx-auto px-6 pt-6">
+        <div class=" flex flex-row  ">
+            <div class="basis-1/4 pl-28">
+                {/* <img src="https://picsum.photos/50/50" alt=""> */}
+            </div>
+            <div class=" basis-1/2 relative px-8  ">
+                <div class="absolute pointer-events-auto py-3 pl-1.5">
+                  <svg class="absolute text-slate-400 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                  </svg>
+                </div>
+                {/* <input type="text" placeholder="Search"  class=" rounded border border-slate-400 border-solid h-10 w-full pl-10"> */}
+            </div>
+
+            <div class="basis-1/4 flex flex-row gap-4 px-20 ">
+                
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"  fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"  fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </svg>
+            </div>
+        </div>
+        <nav class="flex justify-center border-t-2 mt-5 border-current  ">
+            <a href="/dashboard" class="font-medium px-3 py-2  text-slate-700 rounded-lg hover:bg-zinc-500 hover:text-white">TRANG CHỦ</a>
+            <div class=" relative group  flex flex-row  rounded-lg hover:bg-zinc-500 hover:text-white"    >
+              <a href="/team" class="font-medium px-3 py-2 dark:hover:text-white  rounded-lg ">THỜI TRANG NỮ</a>
+                    <ul class="  absolute hidden group-hover:block top-10 left-0 bg-slate-100 rounded-lg text-slate-900  w-36 pl-5 border " >
+                        <li><a href="" class="block "></a>ÁO</li>
+                        <li><a href="" class="block "></a>QUẦN</li>
+                        <li><a href="" class="block "></a>VÁY</li>
+                    </ul>
+            </div>
+            <div class=" relative group  flex flex-row  rounded-lg hover:bg-zinc-500 hover:text-white"    >
+                <a href="/team" class="font-medium px-3 py-2 dark:hover:text-white  rounded-lg ">THỜI TRANG NỮ</a>
+                      <ul class="  absolute hidden group-hover:block top-10 left-0 bg-slate-100 rounded-lg text-slate-900  w-36 pl-5 border " >
+                          <li><a href="" class="block "></a>ÁO</li>
+                          <li><a href="" class="block "></a>QUẦN</li>
+                        
+                      </ul>
+            </div>
+            <a href="/reports" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-zinc-500 hover:text-white">TIN TỨC</a>
+          </nav>
+    </header>
+    
+
+
+
+
+
+
+
+
