9becb43dfaeb82185b41ecdde7c8e30cf1537a8e
BuiFee <buingocphinn@gmail.com>
2023-07-16 18:41:11 +0700

    Hoàng add

---
+++ src/pages/News.js
@@ -0,0 +1,135 @@
+const News = () => {
+  return ` <!-- Container for demo purpose -->
+  <div class="container my-24 mx-auto md:px-6">
+      <!-- Section: Design Block -->
+      <section class="mb-32">
+          <style>
+              .zoom:hover img {
+                  transform: scale(1.1);
+              }
+          </style>
+          <h2 class="mb-12 text-center text-3xl font-bold underline">TIN TỨC</h2>
+
+          <div class="grid gap-6 lg:grid-cols-3">
+              <div class="zoom relative overflow-hidden  bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                  data-te-ripple-init data-te-ripple-color="light">
+                  <img src="https://pos.nvncdn.net/4be5a3-2552/art/20150123_dbvODQrrGFeFRjr5Hrl1fnO3.jpg"
+                      sizes="360px" class="w-full align-middle transition duration-300 ease-linear" />
+                  <a href="#!">
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                          <div class="flex h-full items-end justify-start">
+                              <div class="m-6 text-white">
+                                  <h5 class="mb-3 text-lg font-bold">Chiếc áo dài Việt </h5>
+                                  <p>
+                                      <small>Published <u>13.01.2022</u>by Nguyễn Sĩ Hoàng</small>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]">
+                      </div>
+                  </a>
+              </div>
+
+              <div class="zoom relative overflow-hidden  bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                  data-te-ripple-init data-te-ripple-color="light">
+                  <img src="https://pos.nvncdn.net/4be5a3-2552/art/20190926_sUwLGPCCF6DNZmLkyED60j0Q.jpg"
+                      sizes="360px" class="w-full align-middle transition duration-300 ease-linear" />
+                  <a href="#!">
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                          <div class="flex h-full items-end justify-start">
+                              <div class="m-6 text-white">
+                                  <h5 class="mb-3 text-lg font-bold">
+                                      Biết 4 items công sở được chính các BTV thời trang diện quanh ngày tháng, bạn sẽ
+                                      muốn sắm bằng hết cho tủ đồ
+                                  </h5>
+                                  <p>
+                                      <small>Published <u>12.01.2022</u> by Mark Equel</small>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]">
+                      </div>
+                  </a>
+              </div>
+
+              <div class="zoom relative overflow-hidden  bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                  data-te-ripple-init data-te-ripple-color="light">
+                  <img src="https://pos.nvncdn.net/4be5a3-2552/art/20190926_XPiiE2cwUohaSrdJAOUKM5cs.jpg"
+                      class="w-full align-middle transition duration-300 ease-linear" />
+                  <a href="#!">
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                          <div class="flex h-full items-end justify-start">
+                              <div class="m-6 text-white">
+                                  <h5 class="mb-3 text-lg font-bold">3 quy tắc thời trang giúp bạn đỡ tốn tiền khi đi
+                                      mua sắm mà vẫn mặc đẹp</h5>
+                                  <p>
+                                      <small>Published <u>10.01.2022</u> by Bilbo baggins</small>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]">
+                      </div>
+                  </a>
+              </div>
+
+              <div class="zoom relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                  data-te-ripple-init data-te-ripple-color="light">
+                  <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/047.jpg"
+                      class="w-full align-middle transition duration-300 ease-linear" />
+                  <a href="#!">
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                          <div class="flex h-full items-end justify-start">
+                              <div class="m-6 text-white">
+                                  <h5 class="mb-3 text-lg font-bold">Let's go!</h5>
+                                  <p>
+                                      <small>Published <u>09.01.2022</u> by Halley Frank</small>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]">
+                      </div>
+                  </a>
+              </div>
+
+              <div class="zoom relative overflow-hidden  bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                  data-te-ripple-init data-te-ripple-color="light">
+                  <img src="https://kenh14cdn.com/thumb_w/300/203336854389633024/2023/7/14/photo1689319161310-16893191615821060102489.jpg"
+                      class="w-full align-middle transition duration-300 ease-linear" />
+                  <a href="#!">
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
+                          <div class="flex h-full items-end justify-start">
+                              <div class="m-6 text-white">
+                                  <h5 class="mb-3 text-lg font-bold">
+                                      4 kiểu quần short lỗi mốt, khiến phong cách mùa hè tụt dốc
+                                  </h5>
+                                  <p>
+                                      <small>Published <u>07.01.2022</u> by David Beak</small>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                      <div
+                          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]">
+                      </div>
+                  </a>
+              </div>
+
+
+          </div>
+      </section>`;
+};
+
+export default News;
---
+++ src/pages/Signin.js
@@ -0,0 +1,67 @@
+const Signin = () => {
+  return `
+    <section class="">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0  ">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                    Đăng kí
+                </h1>
+                <form class="space-y-4 md:space-y-6" action="#">
+                    <div>
+
+                        <input type="text" id="username"
+                            class="bg-gray-50 border border-gray-300  text-gray-900     w-full p-2.5  dark:border-gray-400 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="User Name" required="">
+                    </div>
+                    <div>
+                        <label for="password"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Mật khẩu"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-400 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required="">
+                    </div>
+
+                    <div class="flex items-start">
+
+
+                    </div>
+                    <button type="submit" class=" w-full rounded-lg px-5 py-2 text-white bg-orange-500     ">Đăng
+                        kí</button>
+                    <p class="text-sm font-light text-dark-500 dark:text-dark-400">
+                        Bạn đã có tài khoản? <a href="#" class="font-medium text-blue-600 hover:underline ">Đăng
+                            nhập</a>
+                    </p>
+                    <!-- Social login buttons -->
+                    <a class="mb-3 flex w-full items-center justify-center rounded bg-primary px-7 pb-2.5 pt-3 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                        style="background-color: #3b5998" href="#!" role="button" data-te-ripple-init
+                        data-te-ripple-color="light">
+                        <!-- Facebook -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-3.5 w-3.5" fill="currentColor"
+                            viewBox="0 0 24 24">
+                            <path
+                                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+                        </svg>
+                        Continue with Facebook
+                    </a>
+
+                    <button class="w-full group h-12 px-7 pb-2.5 pt-3 border-2 border-gray-300 rounded transition duration-300 
+                    hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
+                        <div class="relative flex items-center space-x-4 justify-center">
+                            <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg"
+                                class="absolute left-10 w-5" alt="google logo">
+                            <span
+                                class="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">Continue
+                                with Google</span>
+                        </div>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+  `;
+};
+
+export default Signin;
